618aa6ee60a32de90ce7cf906cd7c5fd73a25f94
harshith.t <harshith.t@juspay.in>
2024-12-11 18:25:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PipelineWeb from './Components/PipelineWeb/PipelineWeb';
+
+function App() {
+  return (
+    <div >
+     <PipelineWeb/>
+    </div>
+  );
+}
+
+export default App;
